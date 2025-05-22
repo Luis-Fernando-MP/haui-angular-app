d0517ae6b27d58f4d2045c93ce2216932b0bbb42
Juls <luigfmp@gmail.com>
2025-05-22 10:40:41 -0500

    feat: restructure app with new components and routing

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,11 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { RouterModule, RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  imports: [RouterOutlet, RouterModule],
+  templateUrl: './app.html',
+  styleUrl: './app.component.scss',
 })
 export class AppComponent {
   title = 'haui-app-curso';
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,24 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './pages/home/home.component';
+import { GlobeComponent } from './pages/globe/globe.component';
+import { CalculatorComponent } from './pages/calculator/calculator.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    component: HomeComponent,
+  },
+  {
+    path: 'globe',
+    component: GlobeComponent,
+  },
+  {
+    path: 'calculator',
+    component: CalculatorComponent,
+  },
+  {
+    path: '**',
+    redirectTo: '',
+    pathMatch: 'full',
+  },
+];
